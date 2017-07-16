42c1ff6f9153cb419a455feae8c8602c18ed740d
Michiel Overtoom <motoom@xs4all.nl>
2017-07-16 20:07:19 +0200

    Simple script to determine the most prolific twitter users:

---
+++ prolific.py
@@ -0,0 +1,23 @@
+import json
+import collections
+
+twitterers = collections.defaultdict(int)
+
+with open("stream_BhagNawazBhag.json") as f:
+    for line in f:
+        ob = json.loads(line)
+        # text = ob.get("text", "")
+        user = ob.get("user")
+        if user:
+            username = user.get("name", "")
+        if username:
+            twitterers[username] += 1
+
+print "Most prolific twitterers (or tweeters? Twitter users?)"
+
+for k, v in sorted(twitterers.iteritems(), key=lambda item: item[1], reverse=True):
+    msg = u"%s - %d tweets" % (k, v)
+    print msg.encode("utf8")
+    if v < 6:
+        break # We're not interested in people with less than 6 tweets
+
