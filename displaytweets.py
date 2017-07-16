aa7100015719a302de90d126d4f76e35eb385182
Michiel Overtoom <motoom@xs4all.nl>
2017-07-16 20:15:38 +0200

    Display tweets from the stream

---
+++ displaytweets.py
@@ -0,0 +1,17 @@
+import json
+
+nr = 0
+for line in open("data/BhagNawazBhag_stream.txt"):
+    ob = json.loads(line)
+    text = ob.get("text", "")
+    user = ob.get("user")
+    if user:
+        username = user.get("name", "")
+    print username
+    print text.encode("utf8")
+    print
+
+    if nr > 10:
+        break
+    else:
+        nr += 1
